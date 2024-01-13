005eae708035e4e72dab1edbab06cb0119e3fa71
Kapil Goyal <kkgoyal@suyati.com>
2024-01-14 00:14:55 +0530

    Initial Commit

---
+++ SwiftUI-Weather/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI-Weather
+//
+//  Created by Kapil Kumar on 14/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
